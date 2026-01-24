4686282fa04285102899d6fe642d96b2a880d750
krowbee <skoresplay@gmail.com>
2026-01-24 16:34:18 +0200

    feat: added courses page url to CoursesSide.tsx

--- app/(features)/courses/components/CoursesSide.tsx
+++ app/(features)/courses/components/CoursesSide.tsx
@@ -1,9 +1,14 @@
 import { SidebarGroup } from "@/components/ui/sidebar";
+import { COURSES_URL } from "@/urls/courses";
+import Link from "next/link";
 
 export function CoursesSide() {
   return (
     <SidebarGroup className="p-0 gap-2 pt-2 border-t-1 border-b-1">
-      <h3 className="font-accent text-lg text-center">Курси</h3>
+      <h3 className="font-accent text-lg text-center">
+        <Link href={COURSES_URL.courses_page}>Курси</Link>
+      </h3>
+
       <div className="courses-side-container flex flex-col max-h-[110px] overflow-y-auto hide-scrollbar gap-2">
         <div className="w-full flex flex-col px-4 border-1   hover:cursor-pointer">
           <h3 className="text-md font-body">English learning A1</h3>
