2b170a292b5f1034301d0b93ea3af8db2ebe3267
krowbee <skoresplay@gmail.com>
2026-02-09 17:56:04 +0200

    feat: integrated tiptap-react for display and editing lesson theory, integrated shadcn-tooltip for tiptap menubar

--- app/(features)/courses/[courseSlug]/[lessonSlug]/components/TheoryBlock.tsx
+++ app/(features)/courses/[courseSlug]/[lessonSlug]/components/TheoryBlock.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Tiptap } from "@/components/TipTap";
 import { Card, CardDescription, CardHeader } from "@/components/ui/card";
 
 export function TheoryBlock({
@@ -14,119 +15,9 @@ export function TheoryBlock({
       <CardHeader>
         <h1 className="font-heading text-2xl text-center">{lessonName}</h1>
       </CardHeader>
+      <hr className="bg-primary"></hr>
       <CardDescription>
-        {
-          // add tiptap editor
-        }
-        <p className="font-accent text-base text-white tracking-tighter">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-          numquam perspiciatis, necessitatibus eaque porro, officiis ad cum
-          beatae reiciendis, exercitationem explicabo? Sit veritatis, ipsum
-          totam numquam obcaecati dolorum tempora at Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Excepturi numquam perspiciatis,
-          necessitatibus eaque porro, officiis ad cum beatae reiciendis,
-          exercitationem explicabo? Sit veritatis, ipsum totam numquam obcaecati
-          dolorum tempora at Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Excepturi numquam perspiciatis, necessitatibus eaque porro,
-          officiis ad cum beatae reiciendis, exercitationem explicabo? Sit
-          veritatis, ipsum totam numquam obcaecati dolorum tempora at Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam
-          perspiciatis, necessitatibus eaque porro, officiis ad cum beatae
-          reiciendis, exercitationem explicabo? Sit veritatis, ipsum totam
-          numquam obcaecati dolorum tempora at Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Excepturi numquam perspiciatis,
-          necessitatibus eaque porro, officiis ad cum beatae reiciendis,
-          exercitationem explicabo? Sit veritatis, ipsum totam numquam obcaecati
-          dolorum tempora at Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Excepturi numquam perspiciatis, necessitatibus eaque porro,
-          officiis ad cum beatae reiciendis, exercitationem explicabo? Sit
-          veritatis, ipsum totam numquam obcaecati dolorum tempora at Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam
-          perspiciatis, necessitatibus eaque porro, officiis ad cum beatae
-          reiciendis, exercitationem explicabo? Sit veritatis, ipsum totam
-          numquam obcaecati dolorum tempora at Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Excepturi numquam perspiciatis,
-          necessitatibus eaque porro, officiis ad cum beatae reiciendis,
-          exercitationem explicabo? Sit veritatis, ipsum totam numquam obcaecati
-          dolorum tempora at Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Excepturi numquam perspiciatis, necessitatibus eaque porro,
-          officiis ad cum beatae reiciendis, exercitationem explicabo? Sit
-          veritatis, ipsum totam numquam obcaecati dolorum tempora at Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam
-          perspiciatis, necessitatibus eaque porro, officiis ad cum beatae
-          reiciendis, exercitationem explicabo? Sit veritatis, ipsum totam
-          numquam obcaecati dolorum tempora at Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Excepturi numquam perspiciatis,
-          necessitatibus eaque porro, officiis ad cum beatae reiciendis,
-          exercitationem explicabo? Sit veritatis, ipsum totam numquam obcaecati
-          dolorum tempora at Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Excepturi numquam perspiciatis, necessitatibus eaque porro,
-          officiis ad cum beatae reiciendis, exercitationem explicabo? Sit
-          veritatis, ipsum totam numquam obcaecati dolorum tempora at Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam
-          perspiciatis, necessitatibus eaque porro, officiis ad cum beatae
-          reiciendis, exercitationem explicabo? Sit veritatis, ipsum totam
-          numquam obcaecati dolorum tempora at Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Excepturi numquam perspiciatis,
-          necessitatibus eaque porro, officiis ad cum beatae reiciendis,
-          exercitationem explicabo? Sit veritatis, ipsum totam numquam obcaecati
-          dolorum tempora at Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Excepturi numquam perspiciatis, necessitatibus eaque porro,
-          officiis ad cum beatae reiciendis, exercitationem explicabo? Sit
-          veritatis, ipsum totam numquam obcaecati dolorum tempora at Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam
-          perspiciatis, necessitatibus eaque porro, officiis ad cum beatae
-          reiciendis, exercitationem explicabo? Sit veritatis, ipsum totam
-          numquam obcaecati dolorum tempora at Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Excepturi numquam perspiciatis,
-          necessitatibus eaque porro, officiis ad cum beatae reiciendis,
-          exercitationem explicabo? Sit veritatis, ipsum totam numquam obcaecati
-          dolorum tempora at Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Excepturi numquam perspiciatis, necessitatibus eaque porro,
-          officiis ad cum beatae reiciendis, exercitationem explicabo? Sit
-          veritatis, ipsum totam numquam obcaecati dolorum tempora at Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam
-          perspiciatis, necessitatibus eaque porro, officiis ad cum beatae
-          reiciendis, exercitationem explicabo? Sit veritatis, ipsum totam
-          numquam obcaecati dolorum tempora at Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Excepturi numquam perspiciatis,
-          necessitatibus eaque porro, officiis ad cum beatae reiciendis,
-          exercitationem explicabo? Sit veritatis, ipsum totam numquam obcaecati
-          dolorum tempora at Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Excepturi numquam perspiciatis, necessitatibus eaque porro,
-          officiis ad cum beatae reiciendis, exercitationem explicabo? Sit
-          veritatis, ipsum totam numquam obcaecati dolorum tempora at Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam
-          perspiciatis, necessitatibus eaque porro, officiis ad cum beatae
-          reiciendis, exercitationem explicabo? Sit veritatis, ipsum totam
-          numquam obcaecati dolorum tempora at Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Excepturi numquam perspiciatis,
-          necessitatibus eaque porro, officiis ad cum beatae reiciendis,
-          exercitationem explicabo? Sit veritatis, ipsum totam numquam obcaecati
-          dolorum tempora at Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Excepturi numquam perspiciatis, necessitatibus eaque porro,
-          officiis ad cum beatae reiciendis, exercitationem explicabo? Sit
-          veritatis, ipsum totam numquam obcaecati dolorum tempora at Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam
-          perspiciatis, necessitatibus eaque porro, officiis ad cum beatae
-          reiciendis, exercitationem explicabo? Sit veritatis, ipsum totam
-          numquam obcaecati dolorum tempora at Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Excepturi numquam perspiciatis,
-          necessitatibus eaque porro, officiis ad cum beatae reiciendis,
-          exercitationem explicabo? Sit veritatis, ipsum totam numquam obcaecati
-          dolorum tempora at Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Excepturi numquam perspiciatis, necessitatibus eaque porro,
-          officiis ad cum beatae reiciendis, exercitationem explicabo? Sit
-          veritatis, ipsum totam numquam obcaecati dolorum tempora at Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Excepturi numquam
-          perspiciatis, necessitatibus eaque porro, officiis ad cum beatae
-          reiciendis, exercitationem explicabo? Sit veritatis, ipsum totam
-          numquam obcaecati dolorum tempora at Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Excepturi numquam perspiciatis,
-          necessitatibus eaque porro, officiis ad cum beatae reiciendis,
-          exercitationem explicabo? Sit veritatis, ipsum totam numquam obcaecati
-          dolorum tempora at
-        </p>
+        <Tiptap editable={false} />
       </CardDescription>
     </Card>
   );
